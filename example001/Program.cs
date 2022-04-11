a8b069b888478effeea1dbaa48b45d3c3a19bb13
«Vova <vrobskiy@gmail.com>
2022-04-11 23:16:50 +0300

    add First Task done

---
+++ example001/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите трехзначное число: ");
+int inNum = Convert.ToInt32(Console.ReadLine());
+int a = inNum%100;
+int ostDel = a%10;
+Console.Write("Последняя цифра введенного числа это: " + ostDel);
+
