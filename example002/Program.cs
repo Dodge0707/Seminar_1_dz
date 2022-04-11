41c0487306d1204f0d3d69516712aef3b5a4a042
«Vova <vrobskiy@gmail.com>
2022-04-11 23:32:03 +0300

    add Vtora9 zada4a done

---
+++ example002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.Write("Большее число: " + numberA + " ");
+    Console.Write("Меньшее число: " + numberB);
+}
+else 
+{
+    Console.Write("Большее число: " + numberB + " ");
+    Console.Write("Меньшее число: " + numberA);
+}
