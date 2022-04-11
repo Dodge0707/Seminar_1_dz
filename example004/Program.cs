9abf8949d8e907a43f2d5446d4b8f9bdc4b253fa
«Vova <vrobskiy@gmail.com>
2022-04-11 23:42:43 +0300

    add Example 004 done

---
+++ example004/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int ostDel = numberA%2;
+
+if (ostDel == 0)
+{
+    Console.Write("Вы ввели четное число " + numberA + "!");
+}
+else 
+{
+    Console.Write("Вы ввели нечетное число " + numberA + "!!!");
+}
