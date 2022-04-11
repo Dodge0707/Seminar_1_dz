9dc81bd6fa7d20e97738360dd73986c017613a70
«Vova <vrobskiy@gmail.com>
2022-04-12 00:30:40 +0300

    add Example 005 Done

---
+++ Example005/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+//int ostDel = numberA%2;
+int b = 2;
+
+while (b <= numberA)
+{
+    Console.Write(b);
+    b= b +2;
+    if (b <= numberA)
+    {
+        Console.Write(", ");
+    }
+}
+
+
+/* Денис, у меня есть вопрос по этой задаче. Вот такое решение мне подсказал одногруппник. Конеретно то, что надо прибавлять +2. Изнчально мой цикл выглядел так:
+int ostDel = numberA%2;
+while (b <= numberA && ostDel == 0)
+{
+    Console.Write(b);
+    b++;
+    if (b <= numberA)
+    {
+        Console.Write(", ");
+    }
+}
+Но почему-то не работало. Можете подсказать, где ошибка?
+*/
