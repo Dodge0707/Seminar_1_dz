ce989408e9b58924f3a7418b6e129bad91cb36a2
«Vova <vrobskiy@gmail.com>
2022-04-12 22:14:10 +0300

    add Example 006

---
+++ example006/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA < 0)
+{
+    Console.Write(-numberA);
+}
+else
+{
+    Console.Write(numberA);
+}
